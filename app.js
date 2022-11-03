bdf7dcf8f509e477362ada671e73e67e964b14fa
LauraCordoba1 <laurabelen.cordoba@hotmail.com>
2022-11-03 20:58:55 -0300

    rutas

--- app.js
+++ app.js
@@ -28,13 +28,14 @@ app.get("/login2", (req,res) => {
     res.sendFile(path.resolve("./views/login2.html"));
 });
 
-app.get("/", (req, res) => {
+app.get("/login", (req, res) => {
     res.sendFile(path.resolve("./views/login.html"));
   })
   
   app.get("/home", (req, res) => {
     res.sendFile(path.resolve("./views/home.html"));
   })
+  
   
 
 
