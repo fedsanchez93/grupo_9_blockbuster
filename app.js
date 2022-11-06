596dadeb0eaa14b8de4af5bb84b74870248e411f
moshecha <moshechami@gmail.com>
2022-11-05 21:26:00 -0300

    orden de /

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ app.use(express.static('public')); // Recursos estaticos
 app.listen(PORT,() => console.log("Server listening on port  " + PORT));
 
 app.get("/", (req,res) => { 
-    res.sendFile(path.resolve("./views/index.html"));
+    res.sendFile(path.resolve("./views/home.html"));
 });
-
+app.get("/home", (req, res) => {
+    res.sendFile(path.resolve("./views/home.html"));
+  })
 app.get("/carrito", (req,res) => {
     res.sendFile(path.resolve("./views/carrito_main.html"));
 });
@@ -28,13 +30,9 @@ app.get("/login", (req,res) => {
     res.sendFile(path.resolve("./views/login.html"));
 });
 
-app.get("/login", (req, res) => {
-    res.sendFile(path.resolve("./views/login.html"));
-  })
+
   
-  app.get("/home", (req, res) => {
-    res.sendFile(path.resolve("./views/home.html"));
-  })
+
   
   
 
