c5bed9f7c7881f450d13ec116a982b4888e29b44
fedsanchez93 <fed.sanchez@outlook.com>
2022-11-13 19:42:55 -0300

    Se modificaron las vistas incluyendo los partials

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const router = express.Router()
-const path=require('path')
-const app=express()
-const mainController = require('../controllers/mainController')
+const router = express.Router();
+const path = require('path');
+const app = express();
+const mainController = require('../controllers/mainController');
 
-app.use(express.static(path.resolve(__dirname,'./public')))
+app.use(express.static(path.resolve(__dirname,'./public')));
 
-router.get('/',mainController.home)
-router.get('/home',mainController.home)
-router.get('/login',mainController.login)
-router.get('/register',mainController.register)
-router.get('/carrito',mainController.carrito)
-router.get('/carrito2',mainController.carrito2)
-router.get('/productDetail',mainController.productDetail)
-router.get('/administrarProductos',mainController.administrarProductos)
+router.get('/',mainController.home);
+router.get('/home',mainController.home);
+router.get('/login',mainController.login);
+router.get('/register',mainController.register);
+router.get('/carrito',mainController.carrito);
+router.get('/carrito2',mainController.carrito2);
+router.get('/productDetail',mainController.productDetail);
+router.get('/administrarProductos',mainController.administrarProductos);
 
-module.exports=router;
+module.exports = router;
