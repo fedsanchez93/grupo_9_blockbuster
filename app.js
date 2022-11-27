4074efa435652a0516ac2ceaa979992a19e99ca3
moshecha <moshechami@gmail.com>
2022-11-26 23:10:21 -0300

    render

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const router = express.Router()
 
 app.use(express.static('public')); // Recursos estaticos
