f16c440503ab869b74a1ba7dc2a3901b259c230e
moshecha <moshechami@gmail.com>
2022-12-11 08:20:03 -0300

    perfil

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,15 @@
 const path = require('path')
 const fs = require('fs');
 
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const listaUsers = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 const controller = {
     perfil:(req,res)=>{
-        res.render('users/perfilUser')
+        res.render('users/perfilUser',{listaUsers})
     },
     editarPerfil:(req,res)=>{
-        res.render('users/editarPerfilUser')
+        res.render('users/editarPerfilUser',{listaUsers})
     },
 }
 
