58c8098c9b9cf158e7232aacab8d364911ecd105
moshecha <moshechami@gmail.com>
2022-12-11 12:30:21 -0300

    registro

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,6 +11,22 @@ const controller = {
     editarPerfil:(req,res)=>{
         res.render('users/editarPerfilUser',{listaUsers})
     },
+    guardarRegistro:(req,res)=>{
+        let newUser = {
+            id: (listaUsers[listaUsers.length-1].id)+1,
+            name:req.body.name,
+            usuario:req.body.usuario,
+            email:req.body.email,
+            password:req.body.password,
+            image:req.file.filename || '/emiliaclarke.png'
+        }
+        listaUsers.push(newUser)
+        fs.writeFileSync(usersFilePath, JSON.stringify(listaUsers,null, '\t' ))
+
+        console.log(newUser)
+        console.log(req.file.filename)
+        res.redirect('/register')
+    }
 }
 
 module.exports = controller
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,13 +3,25 @@ const router = express.Router();
 const path = require('path');
 const app = express();
 const usersController = require('../controllers/usersController');
+// Multer
+const multer = require('multer') // multer va en routes
+const storage = multer.diskStorage({ 
+    destination: function (req, file, cb) { 
+       cb(null, './public/images/users'); 
+    }, 
+    filename: function (req, file, cb) { 
+       cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  } 
+  })
+  const uploadFile = multer({ storage });
 
 // Atencion que todas las rutas de este router empiezan con /users/
 
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 router.get('/perfil',usersController.perfil)
+router.post('/guardarRegistro', uploadFile.single('imageUser'),  usersController.guardarRegistro)
 router.get('/editarPerfil', usersController.editarPerfil)
+
 
 
 module.exports = router;
