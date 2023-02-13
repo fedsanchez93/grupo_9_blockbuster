8fb5ee43ee808b10b9bb4ace90bbf13e5ad81c16
NixonMB <nicolasm.benedetti@gmail.com>
2023-02-13 08:46:08 -0300

    validación del registro con front

---
+++ public/js/validateRegister.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function() {
+    let formName = document.querySelector('#name');
+    let formUser = document.querySelector('#usuario');
+    let formEmail = document.querySelector('#email');
+    let formPassword = document.querySelector('#password');
+    let formImage = document.querySelector('#file-input');
+    let form = document.querySelector('form.form-registro');
+
+    form.addEventListener('',function(e){
+        e.preventDefault();
+    })
+
+
+})
