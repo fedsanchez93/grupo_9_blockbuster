a5a5fd45ddeac116bbed58e66de04a3ac5d51f3c
NixonMB <nicolasm.benedetti@gmail.com>
2023-02-13 20:50:14 -0300

    validacion de registro en front end

--- public/js/validateRegister.js
+++ public/js/validateRegister.js
@@ -1,13 +1,23 @@
 window.addEventListener('load', function() {
-    let formName = document.querySelector('#name');
-    let formUser = document.querySelector('#usuario');
-    let formEmail = document.querySelector('#email');
-    let formPassword = document.querySelector('#password');
-    let formImage = document.querySelector('#file-input');
+    let name = document.querySelector('#name');
+    let usuario = document.querySelector('#usuario');
+    let email = document.querySelector('#email');
+    let password = document.querySelector('#password');
+    let image = document.querySelector('#file-input');
     let form = document.querySelector('form.form-registro');
+
+    let errorName = document.querySelector()
+
+
 
     form.addEventListener('',function(e){
         e.preventDefault();
+
+        if (name.value.length < 2){
+            error_name.innerHTML = "El nombre debe contener al menos 2 caracteres";
+        } else {
+            error_name.innerHTML = "Nombre aceptable";
+        };
     })
 
 
