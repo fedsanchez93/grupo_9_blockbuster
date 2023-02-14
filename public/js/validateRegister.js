0c6e3f636ee76a7921159145e10273fc32576751
NixonMB <nicolasm.benedetti@gmail.com>
2023-02-13 22:11:12 -0300

    validacion password en register

--- public/js/validateRegister.js
+++ public/js/validateRegister.js
@@ -8,7 +8,8 @@ window.addEventListener('load', function() {
 
     let errorName = document.querySelector('.error_name');
     let errorUser = document.querySelector('.error_user');
-    let errorEmail = document.querySelector('.error_email');
+    let errorPassword = document.querySelector('.error_password ul');
+    <div class="error_user"></div>
 
 
 
@@ -16,26 +17,56 @@ window.addEventListener('load', function() {
         e.preventDefault();
 
         //Validación del nombre
-        if (name.value.length < 2){
+        if(name.value.length < 2){
             errorName.innerHTML = "El nombre debe contener al menos 2 caracteres";
         } else {
             errorName.innerHTML = "Nombres aceptables";
         };
 
         //Validación del usuario
-        if (user.value.length < 2){
+        if(user.value.length < 2){
             errorUser.innerHTML = "El usuario debe contener al menos 2 caracteres";
         } else {
             errorUser.innerHTML = "Nombre de usuario aceptable";
         };
 
-        //Validación del usuario
+        //Validación del email
         let mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
-        if (email.value.match(mailformat)){
+        if(email.value.match(mailformat)){
             errorEmail.innerHTML = "El email no es válido";
         } else {
             errorEmail.innerHTML = "Email aceptable";
         };
+
+        //Validación del password
+        let errorDePassword = [];
+        let lowerCaseLetters = /[a-z]/g;
+        let upperCaseLetters = /[A-Z]/g;
+        let numbers = /[0-9]/g;
+        
+        if(password.value.length < 5){
+            errorDePassword.push = "La contraseña debe contener al menos 5 caracteres";
+        } 
+
+        if(password.value.match(lowerCaseLetters)){
+            errorDePassword.push = "La contraseña debe contener al menos una minúscula";
+        };
+
+       if(password.value.match(upperCaseLetters)){
+            errorDePassword.push = "La contraseña debe contener al menos una mayúscula";
+        };
+
+        if(erroresDePassword.length > 0){
+            for (let i=0; i < erroresDePassword.length; i++){
+                errorPassword.innerHTML += "<li>" + errorDePassword[i] + "</li>";
+            };
+        };
+
+    
+
+        /*if(password.value.match(".*[*.!@#$%^&(){}[]:";'<>,.?/~`_+-=|\\].*")){
+            errorDePassword.push = "La contraseña debe contener al menos una minúscula";
+            };*/
     })
 
 
