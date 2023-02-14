18d976c00432270739e61ccb1231abbff61a38be
NixonMB <nicolasm.benedetti@gmail.com>
2023-02-13 21:35:07 -0300

    vista de registro

--- public/js/validateRegister.js
+++ public/js/validateRegister.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
     let name = document.querySelector('#name');
-    let usuario = document.querySelector('#usuario');
+    let user = document.querySelector('#usuario');
     let email = document.querySelector('#email');
     let password = document.querySelector('#password');
     let image = document.querySelector('#file-input');
@@ -8,6 +8,7 @@ window.addEventListener('load', function() {
 
     let errorName = document.querySelector('.error_name');
     let errorUser = document.querySelector('.error_user');
+    let errorEmail = document.querySelector('.error_email');
 
 
 
@@ -20,11 +21,20 @@ window.addEventListener('load', function() {
         } else {
             errorName.innerHTML = "Nombres aceptables";
         };
+
         //Validación del usuario
         if (user.value.length < 2){
             errorUser.innerHTML = "El usuario debe contener al menos 2 caracteres";
         } else {
             errorUser.innerHTML = "Nombre de usuario aceptable";
+        };
+
+        //Validación del usuario
+        let mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+        if (email.value.match(mailformat)){
+            errorEmail.innerHTML = "El email no es válido";
+        } else {
+            errorEmail.innerHTML = "Email aceptable";
         };
     })
 
