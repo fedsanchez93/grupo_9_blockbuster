53b7875a54c9decb4cd45cb58cf03be224a47e16
fedsanchez93 <fed.sanchez@outlook.com>
2023-02-25 12:54:43 -0300

    Edicion de perfil de administrador corregido

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -262,18 +262,18 @@ const controller = {
 	addRental:(req,res)=>{
 		let id_user = req.params.id_user
 		let id_movie = req.params.id_movie
-		let d = new Date();
+		let date = new Date();
+		let exp_days = 7; // Una semana
 		//console.log('now:',d,'+1:',sumarDias(d, 1));
 		let arrayMoviesToBuy = []
 		console.log(req.body.cartList)
 
-
 		db.MovieUserRental.bulkCreate([{
 			id_movie:id_movie,
 			id_user:id_user,
-			expired_at: sumarDias(d, req.body.dias) //suma un dia con 0  //'2023-02-03T19:23:08.183Z'   
+			expired_at: sumarDias(date, exp_days) //suma un dia con 0  //'2023-02-03T19:23:08.183Z'   
 		}])
-		.then(result=>{console.log('body:',req.body.dias); res.redirect('/products/misAlquileres'); }) 
+		.then(result=>{console.log('body:',exp_days); res.redirect('/products/misAlquileres'); }) 
 		
 		db.MovieUserCart.destroy({ 
 			where:{
