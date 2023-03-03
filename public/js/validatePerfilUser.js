74ce79368661301ee162468e438e07b7ade96e3a
fedsanchez93 <fed.sanchez@outlook.com>
2023-03-02 21:19:41 -0300

    validacion pass en edicion de usuario

--- public/js/validatePerfilUser.js
+++ public/js/validatePerfilUser.js
@@ -1,5 +1,14 @@
 window.addEventListener('load', function() {
 
+    let password = document.querySelector('#newPassword');
+    let submit = document.querySelector('.boton-guardar');
+
+    let errorPassword = document.querySelector('.error_password');
+    let cantidad = document.querySelector('.error_password .cantidad');
+    let mayusculas = document.querySelector('.error_password .mayusculas');
+    let minusculas = document.querySelector('.error_password .minusculas');
+    let numeros = document.querySelector('.error_password .numeros');
+
     let imageUser = document.querySelector('#file-input')
     imageUser.addEventListener('change',(e)=>{ //para visualizar la imagen antes de subirla
         if(!imageUser.files.length) return // Verificamos si existe una imagen seleccionada
@@ -8,5 +17,57 @@ window.addEventListener('load', function() {
         objectURL = URL.createObjectURL(file);//Creamos la url
         $imgPreview.src = objectURL; 	//Modificamos el atributo src de la etiqueta img
     })
+
+    submit.addEventListener('click', function(event){
+
+        let totalErrores = []
+       
+        //Validación del password
+        let erroresDePassword = [];
+        let lowerCaseLetters = /[a-z]/g;
+        let upperCaseLetters = /[A-Z]/g;
+        let numbers = /[0-9]/g;
+        
+        if(password.value.length < 5){
+            cantidad.innerHTML ="La contraseña debe contener al menos 5 caracteres";
+            cantidad.style.color="red"
+            totalErrores.push(1);
+        } else {
+            cantidad.innerHTML = "La contraseña tiene al menos 5 caracteres";
+            cantidad.style.color = "green"
+        }; 
+
+        if(password.value.match(lowerCaseLetters)){
+            minusculas.innerHTML = "La contraseña tiene minúsculas";
+            minusculas.style.color = "green"
+        } else {
+            minusculas.innerHTML ="La contraseña debe contener al menos una minúscula";
+            minusculas.style.color="red"
+            totalErrores.push(1);
+        }
+        
+       if(password.value.match(upperCaseLetters)){
+            mayusculas.innerHTML = "La contraseña tiene mayúsculas";
+            mayusculas.style.color = "green"
+        } else {
+            mayusculas.innerHTML = "La contraseña debe contener al menos una mayúscula";
+            mayusculas.style.color="red"
+            totalErrores.push(1);
+        }
+
+        if(password.value.match(numbers)){
+            numeros.innerHTML = "La contraseña tiene números";
+            numeros.style.color = "green"        
+        } else {
+            numeros.innerHTML="La contraseña debe contener al menos un número";
+            numeros.style.color="red";
+            totalErrores.push(1);
+        };
+
+        if(totalErrores.length > 0){
+            event.preventDefault();
+        }
+
+    });
 
 })
