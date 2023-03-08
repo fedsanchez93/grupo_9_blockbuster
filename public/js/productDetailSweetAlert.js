cdebf16cf31fa0701b9d28f7b33db7364c747984
moshecha <moshechami@gmail.com>
2023-03-08 16:47:29 -0300

    sweetAlert

--- public/js/productDetailSweetAlert.js
+++ public/js/productDetailSweetAlert.js
@@ -1,12 +1,45 @@
 
+window.addEventListener('load',()=>{
 
-document.querySelector('#agregarDeseos').addEventListener('click',(e)=>{
-e.preventDefault()
+let agregarDeseos = document.querySelector('#agregarDeseos')
+
+agregarDeseos.addEventListener('click',(e)=>{
+    var queryString = window.location.search;
+    var urlParams = new URLSearchParams(queryString);
+    var id_movie = urlParams.get('id');
+    let id_user = agregarDeseos.value 
+	let settings = {
+		method:'POST',
+		headers:{
+			'Content-Type':'application/json',
+		},
+	}
+fetch(`http://localhost:5000/api/wishes/add/${id_user}/${id_movie}/`, settings)
+.then(function(response){
+	return response.json();
+})
+.then(function(data){
+	console.log(data)
     Swal.fire({
         position: 'top-end',
         icon: 'success',
-        title: 'Your work has been saved',
+        title: 'Pelicula Agregada a Mis Deseos!',
         showConfirmButton: false,
         timer: 1500
       })
 })
+.catch(function(error){
+	console.error(error)
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Ocurrio un error',
+        showConfirmButton: false,
+        timer: 1500
+      })
+})
+
+    
+})
+
+})//fin funcion load
