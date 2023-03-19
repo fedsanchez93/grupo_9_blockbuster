c7459e500e62cba775b96ab89f67634955617010
moshecha <moshechami@gmail.com>
2023-03-19 10:22:45 -0300

    config.js

---
+++ src/database/config/config.js
@@ -0,0 +1,24 @@
+
+module.exports={
+    "development": {
+      "username": "root", 
+      "password": '', 
+      "database": "blockbuster_db",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": '',
+      "database": "blockbuster_db",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production": {
+      "username": "root",
+      "password": '',
+      "database": "blockbuster_db",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+  }
